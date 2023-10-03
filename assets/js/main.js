bfa0e1ad30c0b5a4296a877f229b1a6bf5e69870
Andrei Robson da Silva <silva.andrei@outlook.com.br>
2023-10-03 19:52:14 -0300

    correção no main

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,7 +43,7 @@ function updatePortfolio(profileData) {
     portfolio.innerHTML = profileData.portfolio.map(project =>
         `<li>
         <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
-        <a href="${project.url}" target="_blank">${project.url}<a>
+        <a href="${project.url}" target="_blank">${project.url}</a>
         </li>`
     ).join('')
 }
